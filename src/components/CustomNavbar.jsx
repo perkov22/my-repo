e997da19bae5d35840570e61f7c2cf3277e4a5b5
perkov22 <45461622+perkov22@users.noreply.github.com>
2019-01-31 20:35:28 +0100

    Update CustomNavbar.jsx

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -24,10 +24,10 @@ export default class CustomNavbar extends Component {
           <li> <Link className="link" eventKey={1} componentClass={Link}  href="/" to="/">
             <p className="p-nav">HOME</p>
             </Link></li>
-           <li> <Link className="link" eventKey={2} componentClass={Link} href="/about" to="/about">
+           <li> <Link className="link" eventKey={2} componentClass={Link} href="/about" to="/https://innovation-code.netlify.com/about">
               <p className="p-nav">CONTACT</p>
             </Link></li>
-           <li> <Link className="link" eventKey={3} componentClass={Link} href="/news" to="/news">
+           <li> <Link className="link" eventKey={3} componentClass={Link} href="/news" to="">
             <p className="p-nav">SERVICES</p>
             </Link></li>
             </ul>
