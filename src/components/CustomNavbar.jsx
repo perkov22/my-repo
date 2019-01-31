d20a1f127ce97f4293e3ea2f3deaefee503a43a4
Filip Perkov <perkov.fp@gmail.com>
2019-01-31 15:03:35 +0100

    Create a React app and publish it to GitHub Pages

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -24,7 +24,7 @@ export default class CustomNavbar extends Component {
           <li> <Link className="link" eventKey={1} componentClass={Link}  href="/" to="/">
             <p className="p-nav">HOME</p>
             </Link></li>
-           <li> <Link className="link" eventKey={2} componentClass={Link} href="/about" to="/about" target="_self">
+           <li> <Link className="link" eventKey={2} componentClass={Link} href="/about" to="/about">
               <p className="p-nav">CONTACT</p>
             </Link></li>
            <li> <Link className="link" eventKey={3} componentClass={Link} href="/news" to="/news">
