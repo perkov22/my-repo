45b2e15c47611ccbb8f7d8ebcd400b32dd3d4893
Filip Perkov <perkov.fp@gmail.com>
2019-01-31 15:38:29 +0100

    Updates

---
+++ precache-manifest.a1bff27616bd613a4941977e3ade82d2.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "519c6d7a6c95621c9d97",
+    "url": "/my-repo/static/css/main.73fe6a5d.chunk.css"
+  },
+  {
+    "revision": "519c6d7a6c95621c9d97",
+    "url": "/my-repo/static/js/main.519c6d7a.chunk.js"
+  },
+  {
+    "revision": "5bef102dff0e49118888",
+    "url": "/my-repo/static/js/1.5bef102d.chunk.js"
+  },
+  {
+    "revision": "823efce78ec53a25fb0b",
+    "url": "/my-repo/static/js/runtime~main.823efce7.js"
+  },
+  {
+    "revision": "3b22e0a00c90a97ceff60c2a1e5b1cb5",
+    "url": "/my-repo/static/media/pexels3.3b22e0a0.jpg"
+  },
+  {
+    "revision": "7ec27d5a3e8143bea6ac249ddb24bb84",
+    "url": "/my-repo/static/media/ranking.7ec27d5a.jpg"
+  },
+  {
+    "revision": "48edad292311db483b16622d9f8c307b",
+    "url": "/my-repo/static/media/computer.48edad29.jpg"
+  },
+  {
+    "revision": "67b93259785207d7fda26c199f91a477",
+    "url": "/my-repo/static/media/tools.67b93259.png"
+  },
+  {
+    "revision": "8a3fd5765d7ed8076c352bbd8863ddda",
+    "url": "/my-repo/static/media/IMG.8a3fd576.png"
+  },
+  {
+    "revision": "3ca62196d1f7d36f522addb80e4215e4",
+    "url": "/my-repo/static/media/pexels1.3ca62196.jpeg"
+  },
+  {
+    "revision": "6b1f502f7579eba510da98a8cf6b9b4e",
+    "url": "/my-repo/static/media/pexels2.6b1f502f.jpeg"
+  },
+  {
+    "revision": "2d01dd8b4a9264c508e4222c0d1ac773",
+    "url": "/my-repo/static/media/pexels5.2d01dd8b.jpeg"
+  },
+  {
+    "revision": "277d4e98bc3d43df6cdc2b4269c1d1a0",
+    "url": "/my-repo/static/media/main3.277d4e98.jpeg"
+  },
+  {
+    "revision": "8a3568f662675ae4f7177f6c0ed107b8",
+    "url": "/my-repo/static/media/banner2.8a3568f6.png"
+  },
+  {
+    "revision": "16151caca691810f49a9e83085129cd8",
+    "url": "/my-repo/static/media/background3.16151cac.jpg"
+  },
+  {
+    "revision": "6542d125fd773bb136d7ae675d0ed29b",
+    "url": "/my-repo/static/media/s-background2.6542d125.jpg"
+  },
+  {
+    "revision": "84ca1ef37c4e6266bfe3a1f4f458e132",
+    "url": "/my-repo/static/media/background11.84ca1ef3.jpg"
+  },
+  {
+    "revision": "e8db8aed981f906bfa98fffcb212cc8d",
+    "url": "/my-repo/index.html"
+  }
+];
